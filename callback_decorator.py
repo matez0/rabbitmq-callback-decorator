3f72b3af22ba0039657c83c3dfc6bd34a9a4103e
Zoltán Máté <mate.zoltan0@gmail.com>
2023-02-12 23:40:23 +0100

    Extend callback decorator with message parsing

--- callback_decorator.py
+++ callback_decorator.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
+from functools import singledispatchmethod
+import json
+from typing import Union
+
+from pydantic import BaseModel, ValidationError
 
 
-class Callback(ABC):
+class ErrorHandlerCallback(ABC):
     class FatalError(Exception):
         pass
 
@@ -12,14 +17,13 @@ class Callback(ABC):
         self.do_callback = do_callback
 
     def __call__(self, channel, method_frame, header_frame, body):
-
         try:
             self.do_callback(body, header_frame.headers)
 
-        except Callback.FatalError:
+        except self.FatalError:
             self.reject_message(channel, method_frame, header_frame, body)
 
-        except Callback.TryAgainError:
+        except self.TryAgainError:
             self.resend_message_later(channel, method_frame, header_frame, body)
 
         else:
@@ -36,3 +40,56 @@ class Callback(ABC):
     @abstractmethod
     def acknowledge_message(channel, method_frame, header_frame, body):
         pass
+
+
+class JsonCallback:
+    def __init__(self, do_callback):
+        self.do_callback = do_callback
+
+    def __call__(self, body, headers):
+        try:
+            body = json.loads(body)
+
+        except json.JSONDecodeError as exc:
+            raise ErrorHandlerCallback.FatalError(exc)
+
+        self.do_callback(body, headers)
+
+
+class ModelCallback:
+    def __init__(self, *message_types):
+        self.message_types = message_types
+
+    def __call__(self, do_callback):
+        def wrapper(body, headers):
+            class Messages(BaseModel):
+                message: Union[self.message_types]
+
+            try:
+                body = Messages(message=body).message
+
+            except ValidationError as exc:
+                raise ErrorHandlerCallback.FatalError(exc)
+
+            do_callback(body, headers)
+
+        return wrapper
+
+
+class CallbackMeta(type(ABC)):
+    @singledispatchmethod
+    def __call__(cls, do_callback: callable):
+        return super().__call__(do_callback)
+
+    @__call__.register
+    def _(cls, message_type: type(BaseModel), *message_types):
+        factory = super().__call__
+
+        def wrap(do_callback):
+            return factory(JsonCallback(ModelCallback(message_type, *message_types)(do_callback)))
+
+        return wrap
+
+
+class Callback(ErrorHandlerCallback, metaclass=CallbackMeta):
+    pass
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
+import json
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
+
+from pydantic import BaseModel, NegativeInt
 
 from callback_decorator import Callback
 
 
-class TestCallbackDecorator(TestCase):
+class TestCallbackDecoratorBase(TestCase):
     def setUp(self):
         class DerivedCallback(Callback):
             reject_message = Mock()
@@ -16,10 +19,12 @@ class TestCallbackDecorator(TestCase):
         self.callback = DerivedCallback(self.callback_function)
 
         self.headers = {'header-key': 'header-value'}
-        self.body = 'body'
+        self.body = '"body"'
 
         self.callback_args = ('channel', 'method-frame', Mock(headers=self.headers), self.body)
 
+
+class TestErrorHandlerCallbackDecorator(TestCallbackDecoratorBase):
     def test_callback_class_can_be_used_as_decorator(self):
         decorator = type(self.callback)
 
@@ -61,3 +66,82 @@ class TestCallbackDecorator(TestCase):
         self.callback.acknowledge_message.assert_not_called()
         self.callback.reject_message.assert_not_called()
         self.callback.resend_message_later.assert_called_once_with(*self.callback_args)
+
+
+@patch('callback_decorator.json.loads')
+class TestModelCallbackDecorator(TestCallbackDecoratorBase):
+    class MyMessage(BaseModel):
+        myField: float
+
+    def setUp(self):
+        super().setUp()
+
+        decorator = type(self.callback)
+
+        @decorator(self.MyMessage)
+        def callback(*args):
+            self.callback_function(*args)
+
+        self.callback = callback
+
+        self.message = b'{"myField": 3.14}'
+        self.callback_args = *self.callback_args[:3], self.message
+
+    def test_parsed_message_is_passed_when_message_type_is_given(self, json_loads):
+        parsed_message = self.MyMessage.parse_raw(self.message)
+        json_loads.return_value = parsed_message.dict()
+
+        self.callback(*self.callback_args)
+
+        self.callback_function.assert_called_once_with(parsed_message, self.headers)
+
+        self.callback.acknowledge_message.assert_called_once_with(*self.callback_args)
+
+    def test_parsed_message_is_passed_when_multiple_message_types_are_given(self, json_loads):
+        decorator = type(self.callback)
+
+        class MyFirstMessage(BaseModel):
+            myFirstField: bool
+
+        class MyLastMessage(BaseModel):
+            myLastField: NegativeInt
+
+        @decorator(MyFirstMessage, self.MyMessage, MyLastMessage)
+        def callback(*args):
+            self.callback_function(*args)
+
+        parsed_message = self.MyMessage.parse_raw(self.message)
+        json_loads.return_value = parsed_message.dict()
+
+        callback(*self.callback_args)
+
+        self.callback_function.assert_called_once_with(parsed_message, self.headers)
+
+        self.callback.acknowledge_message.assert_called_once_with(*self.callback_args)
+
+    def test_message_shall_be_rejected_when_it_is_not_json(self, json_loads):
+        json_loads.side_effect = json.JSONDecodeError('error-message1', 'doc', 123)
+
+        self.callback(*self.callback_args)
+
+        self.callback_function.assert_not_called()
+
+        json_loads.assert_called_once_with(self.message)
+
+        self.callback.acknowledge_message.assert_not_called()
+        self.callback.reject_message.assert_called_once_with(*self.callback_args)
+        self.callback.resend_message_later.assert_not_called()
+
+    def test_message_shall_be_rejected_when_it_is_not_parsable(self, json_loads):
+        message = b'{"myOtherField": "my-value"}'
+        json_loads.return_value = {"myOtherField": "my-value"}
+
+        callback_args = *self.callback_args[:3], message
+
+        self.callback(*callback_args)
+
+        self.callback_function.assert_not_called()
+
+        self.callback.acknowledge_message.assert_not_called()
+        self.callback.reject_message.assert_called_once_with(*callback_args)
+        self.callback.resend_message_later.assert_not_called()
